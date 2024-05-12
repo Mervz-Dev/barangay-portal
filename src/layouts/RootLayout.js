f421d3b0c5cd2a89ac9e8b2f671f40eadae2912d
Mervin Narvaez <>
2024-05-12 15:58:38 +0800

    added navigation

---
+++ src/layouts/RootLayout.js
@@ -0,0 +1,66 @@
+import { Outlet, NavLink, useNavigate } from "react-router-dom";
+import {
+  Flex,
+  Typography,
+  Layout,
+  Space,
+  Menu,
+  Row,
+  Avatar,
+  Divider,
+} from "antd";
+
+const { Header, Content } = Layout;
+
+const navigation = [
+  { label: "Home", key: "/" },
+  { label: "Services", key: "/services" },
+  { label: "Announcements", key: "/announcements" },
+  { label: "Contact Us", key: "/about" },
+];
+
+export default function RootLayout() {
+  const navigate = useNavigate();
+
+  const handleMenuClick = ({ key }) => {
+    if (key) {
+      navigate(key);
+    }
+  };
+  return (
+    <Layout>
+      <Header
+        style={{
+          display: "flex",
+          height: 85,
+          alignItems: "center",
+          backgroundColor: "#00264d",
+          padding: "0 0",
+        }}
+      >
+        <Row style={{ alignItems: "center", padding: "0 20px" }}>
+          <Space size={10}>
+            <Avatar alt="none" size={65} src={require("../assets/logo.png")} />
+            <Typography.Text style={{ fontSize: 20, color: "white" }}>
+              A-Barangay Hub
+            </Typography.Text>
+          </Space>
+        </Row>
+
+        <Divider type="vertical" />
+
+        <Menu
+          className="menu"
+          style={{ flex: 1, minWidth: 0, borderBottom: "none" }}
+          mode="horizontal"
+          defaultSelectedKeys={["/"]}
+          items={navigation}
+          onClick={handleMenuClick}
+        />
+      </Header>
+      <Content>
+        <Outlet />
+      </Content>
+    </Layout>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import { Flex, Typography, Layout } from "antd";
+
+function Home() {
+  return (
+    <div>
+      <h2>Home Layout</h2>
+    </div>
+  );
+}
+
+export default Home;
