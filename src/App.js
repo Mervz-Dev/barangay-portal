f08e1c9b4a9a784c0aff9b8a29a0f013362c034b
Mervin Narvaez <>
2024-05-12 10:55:13 +0800

    added antd UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Button,
+  Flex,
+  Input,
+  Typography,
+  Layout,
+  Image,
+  Menu,
+  Space,
+} from "antd";
 
+const { Header, Content, Footer } = Layout;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout>
+      <Header
+        style={{
+          height: 80,
+          display: "flex",
+          alignItems: "center",
+          backgroundColor: "#00264d",
+        }}
+      >
+        <Image
+          alt="none"
+          width={50}
+          height={50}
+          src={require("./assets/logo.png")}
+        />
+        <Typography.Text
+          style={{ fontSize: 20, color: "white", fontWeight: "bold" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          A-Barangay Hub
+        </Typography.Text>
+        <Flex style={{ marginLeft: 10 }}>
+          <Space
+            direction="horizontal"
+            size="small"
+            style={{
+              display: "flex",
+            }}
+          >
+            <Typography.Text style={{ color: "white" }}>
+              Welcome to
+            </Typography.Text>
+            <Typography.Text style={{ color: "white" }}>
+              Barangay
+            </Typography.Text>
+          </Space>
+        </Flex>
+      </Header>
+      <Flex style={{ backgroundColor: "red", padding: 10 }}>
+        <Button style={{ flex: 1 }} type="primary">
+          Button
+        </Button>
+        <Button style={{ flex: 1 }} type="primary">
+          Button
+        </Button>
+        <Flex vertical style={{ padding: 10 }}>
+          <Input />
+          <Button type="primary">Button</Button>
+        </Flex>
+      </Flex>
+    </Layout>
   );
 }
 
